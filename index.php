ea85f6f2cf1da5c710395ac1e5e6557a3afd6f36
Pedro V.R. <ptreyes2@gmail.com>
2024-04-04 23:06:12 -0400

    Completed index.php and process.php, fixing some styling issues as well

--- index.php
+++ index.php
@@ -7,24 +7,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cupcake Form</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-    <h1>Cupcake Fundraiser</h1>
-    <h2>Pedro:</h2>
 </head>
 <body>
 
-<form action="process_order.php" method="post">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required><br><br>
+<div class="container">
+    <h1>Cupcake Fundraiser</h1>
+    <h2>Pedro:</h2>
+    <form action="process.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
 
-    <label>Cupcake Flavors:</label><br>
-    <input type="checkbox" name="flavors[]" value="Chocolate">Chocolate<br>
-    <input type="checkbox" name="flavors[]" value="Vanilla">Vanilla<br>
-    <input type="checkbox" name="flavors[]" value="Strawberry">Strawberry<br>
-    <input type="checkbox" name="flavors[]" value="Red Velvet">Red Velvet<br>
-    <input type="checkbox" name="flavors[]" value="Lemon">Lemon<br><br>
+        <label>Cupcake Flavors:</label><br>
+        <input type="checkbox" name="flavors[]" value="The Grasshopper">The Grasshopper<br>
+        <input type="checkbox" name="flavors[]" value="Whiskey Maple Bacon">Whiskey Maple Bacon<br>
+        <input type="checkbox" name="flavors[]" value="Carrot Walnut">Carrot Walnut<br>
+        <input type="checkbox" name="flavors[]" value="Salted Caramel Cupcake">Salted Caramel Cupcake<br>
+        <input type="checkbox" name="flavors[]" value="Red Velvet">Red Velvet<br>
+        <input type="checkbox" name="flavors[]" value="Lemon Drop">Lemon Drop<br>
+        <input type="checkbox" name="flavors[]" value="Tiramisu">Tiramisu<br><br>
 
-    <input type="submit" value="Order">
-</form>
+        <input type="submit" value="Order">
+    </form>
+</div>
 
 </body>
 </html>
--- process.php
+++ process.php
@@ -1,1 +1,43 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $cupcake_flavors = isset($_POST['flavors']) ? $_POST['flavors'] : [];
+
+    // Define cupcake prices
+    $cupcake_prices = [
+        "The Grasshopper" => 3.50,
+        "Whiskey Maple Bacon" => 3.50,
+        "Carrot Walnut" => 3.50,
+        "Salted Caramel Cupcake" => 3.50,
+        "Red Velvet" => 3.50,
+        "Lemon Drop" => 3.50,
+        "Tiramisu" => 3.50
+    ];
+
+    // Calculate total amount
+    $total_amount = 0;
+    $ordered_cupcakes = [];
+    foreach ($cupcake_flavors as $flavor) {
+        if (isset($cupcake_prices[$flavor])) {
+            $total_amount += $cupcake_prices[$flavor];
+            $ordered_cupcakes[] = $flavor;
+        }
+    }
+
+    // Thank you message
+    echo "<h2>Thank You, $name!</h2>";
+    echo "<p>You have ordered the following cupcakes:</p>";
+    echo "<ul>";
+    foreach ($ordered_cupcakes as $cupcake) {
+        echo "<li>$cupcake</li>";
+    }
+    echo "</ul>";
+    echo "<p>Your total amount is: $" . number_format($total_amount, 2) . "</p>";
+} else {
+    // Redirect if accessed directly
+    header("Location: index.php");
+    exit;
+}
+?>
